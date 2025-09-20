63087a8f524032f34ef1fbdf415a7efbff19d79d
NTiger07 <falaleru@gmail.com>
2025-09-20 16:24:31 +0100

    Initial Commit

---
+++ src/main/java/com/favour/linktree_backend/LinktreeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.favour.linktree_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinktreeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinktreeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/favour/linktree_backend/LinktreeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favour.linktree_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinktreeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
